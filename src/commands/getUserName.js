dabfc495cc0031e1513a0d371c6f4399fb325c23
Lapitskiy <hiimisha@mail.ru>
2024-02-02 16:55:08 +0300

    feat: add functions for start

--- src/commands/getUserName.js
+++ src/commands/getUserName.js
@@ -1,8 +1,9 @@
 export function getUserName() {
+  const KEY_USERNAME = '--username='
+
   const args = process.argv.slice(2);
-  const usernameArg = args.find(arg => arg.startsWith('--username='));
+  const usernameArg = args?.find(arg => arg.startsWith(KEY_USERNAME));
+  const name = usernameArg?.substring(KEY_USERNAME.length) || 'Anonymous';
 
-  return usernameArg;
+  return name;
 }
-
-export default getUserName
---
+++ src/utils/printPath.js
@@ -0,0 +1,5 @@
+const printPath = (path) => {
+  console.log(`You are currently in ${path}`)
+}
+
+export default printPath
