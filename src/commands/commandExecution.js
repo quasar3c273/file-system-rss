582e9951d8f7c3500a58805b4c19ef2b74cd3572
Lapitskiy <hiimisha@mail.ru>
2024-02-05 21:51:52 +0300

    feat: add "move file" function

--- src/commands/commandExecution.js
+++ src/commands/commandExecution.js
@@ -6,6 +6,8 @@ import {ERROR_INPUT} from "../consts.js";
 import catCommand from "./operationsWithFiles/cat.js";
 import create from "./operationsWithFiles/add.js";
 import rnFunction from "./operationsWithFiles/rn.js";
+import cpFunction from "./operationsWithFiles/cp.js";
+import mvFunction from "./operationsWithFiles/mv.js";
 
 const commandExecution = async (inputConsole) => {
   const command = inputConsole.trim().split(' ')[0]
@@ -13,33 +15,34 @@ const commandExecution = async (inputConsole) => {
   switch (command) {
     case 'up': {
       await upPath()
-
       break;
     }
     case 'cd': {
       await cdFunction(inputConsole)
-
       break;
     }
     case 'ls': {
       await lsCommand(pathNow)
-
       break;
     }
     case 'cat': {
       await catCommand(inputConsole)
-
       break;
     }
     case 'add': {
       await create(inputConsole);
-
       break;
     }
     case 'rn':
       await rnFunction(inputConsole);
-
       break;
+    case 'cp':
+      await cpFunction(inputConsole);
+      break;
+    case 'mv':
+      await mvFunction(inputConsole);
+      break;
+    // Operating system
     default: {
       throw new Error(ERROR_INPUT);
     }
---
+++ src/commands/operationsWithFiles/mv.js
@@ -0,0 +1,35 @@
+import {access} from "fs/promises"
+import {basename, join} from 'path'
+import {getTargetPath} from "../path/upPath.js"
+import {createReadStream, createWriteStream, unlink} from 'fs'
+import printError from "../../utils/printError.js"
+
+const mvFunction = async (command) => {
+    const newPath = command.trim().replace('mv ', '')
+    const from = newPath.split(' ')[0]
+    const to = newPath.split(' ')[1]
+
+    const fromFilePath = getTargetPath(from)
+    const fileName = basename(fromFilePath)
+    const toFolderPath = getTargetPath(to)
+    const toFilePath = join(`${toFolderPath}`, `${fileName}`)
+
+    try {
+        await access(fromFilePath)
+
+        const readStream = createReadStream(fromFilePath)
+        const writeStream = createWriteStream(toFilePath)
+
+        readStream.pipe(writeStream)
+
+        readStream.on('end', () => {
+            unlink(fromFilePath, (err) => {
+                if (err) throw err;
+            });
+        });
+    } catch (err) {
+        printError(err)
+    }
+};
+
+export default mvFunction
