c1efc6eee76a4f44a596bab5616b6b8d9afeead9
Lapitskiy <hiimisha@mail.ru>
2024-02-05 21:09:58 +0300

    feat: add "copy file" function

---
+++ src/commands/operationsWithFiles/cp.js
@@ -0,0 +1,29 @@
+import { access } from "fs/promises";
+import {basename, join} from 'path';
+import {getTargetPath} from "../path/upPath.js";
+import { createReadStream, createWriteStream } from 'fs';
+import printError from "../../utils/printError.js";
+
+const cpFunction = async (command) => {
+    const newPath = command.trim().replace('cp ', '')
+    const from = newPath.split(' ')[0]
+    const to = newPath.split(' ')[1]
+
+    const fromFilePath = getTargetPath(from)
+    const fileName = basename(fromFilePath)
+    const toFolderPath = getTargetPath(to)
+    const toFilePath = join(`${toFolderPath}`, `${fileName}`)
+
+    try {
+        await access(fromFilePath)
+
+        const readStream = createReadStream(fromFilePath)
+        const writeStream = createWriteStream(toFilePath)
+
+        readStream.pipe(writeStream)
+    } catch (err) {
+        printError(err)
+    }
+};
+
+export default cpFunction
